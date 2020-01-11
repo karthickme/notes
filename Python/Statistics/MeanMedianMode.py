5e09c8704b8cbebdfd7bbc9cebc2e7fa324d0f17
Karthick Elangovan <smekarthick2005@gmail.com>
2020-01-12 00:54:11 +0530

    11th Jan

---
+++ Python/Statistics/MeanMedianMode.py
@@ -0,0 +1,9 @@
+import numpy as np
+import statistics
+
+num = [9.2,10.7,6.8,9,3.4,5.7,5.7]
+print(statistics.mean(num))
+print(statistics.median(num))
+print(statistics.stdev(num))
+print(statistics.variance(num))
+print(statistics.mode(num))
